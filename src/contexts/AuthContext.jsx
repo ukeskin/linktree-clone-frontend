165ea49bded8be9261c6f51ee035c2baa23835a7
Umut Keskin <ukeskin2626@gmail.com>
2022-11-27 17:53:37 +0300

    Add Logout

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "../api/axios";
 import { createContext, useContext } from "react";
+import toast from "react-hot-toast";
 
 export const AuthContext = createContext();
 
@@ -18,12 +19,25 @@ export const AuthProvider = ({ children }) => {
       });
   };
 
+  const signOut = () => {
+    localStorage.removeItem("token");
+    setUser(null);
+
+    setTimeout(() => {
+      window.location.reload();
+    }, 1000);
+
+    toast.success("Signed out successfully", {
+      duration: 2000,
+    });
+  };
+
   useEffect(() => {
     getUser();
   }, []);
 
   return (
-    <AuthContext.Provider value={{ user, getUser }}>
+    <AuthContext.Provider value={{ user, getUser, signOut }}>
       {children}
     </AuthContext.Provider>
   );
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import { useAuth } from "../contexts/AuthContext";
 
 export default function Landing() {
   const navigateTo = useNavigate();
+  const { user, signOut } = useAuth();
   return (
     <div className="flex h-screen w-full">
       <div className="m-auto text-center gap-8 flex flex-col items-center">
@@ -12,14 +14,37 @@ export default function Landing() {
         <p className="text-2xl font-medium text-gray-600">
           A simple link sharing app
         </p>
-        <div className="flex gap-2 items-center">
-          <button className="bg-slate-500 hover:bg-slate-700 text-white font-bold py-2 px-4 rounded">
-            Login
-          </button>
-          <button className="bg-slate-500 hover:bg-slate-700 text-white font-bold py-2 px-4 rounded">
-            <a href="/register">Register</a>
-          </button>
-        </div>
+        {user ? (
+          <div className="flex gap-2 items-center">
+            <button
+              onClick={() => signOut()}
+              className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+            >
+              Logout
+            </button>
+            <button
+              onClick={() => navigateTo("/mylists")}
+              className="bg-slate-500 hover:bg-slate-700 text-white font-bold py-2 px-4 rounded"
+            >
+              My Lists
+            </button>
+          </div>
+        ) : (
+          <div className="flex gap-2 items-center">
+            <button
+              onClick={() => navigateTo("/login")}
+              className="bg-slate-500 hover:bg-slate-700 text-white font-bold py-2 px-4 rounded"
+            >
+              Login
+            </button>
+            <button
+              onClick={() => navigateTo("/register")}
+              className="bg-slate-500 hover:bg-slate-700 text-white font-bold py-2 px-4 rounded"
+            >
+              Register
+            </button>
+          </div>
+        )}
       </div>
     </div>
   );
